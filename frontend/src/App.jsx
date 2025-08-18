1d11a6c82f927c64e7dcdd79e6a5c8de5fd40490
Iviwe Booi <booiiviwe4394@gmail.com>
2025-08-18 23:49:10 +0200

    navbar and landingPage updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,22 +7,31 @@ import LandingPage from './pages/landingPage'
 import ContactPage from './pages/contactPage'
 import LearnPage from './pages/learnPage'
 import ContentDetectPage from './pages/contentDetectPage'
+import Layout from './layout'
+import ErrorPage from './pages/errorPage'
 
 
-function App() {
-  const routes = [
+  const routes = [{
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
     { path: '/', element: <LandingPage /> },
     { path: '/contact', element: <ContactPage /> },
     { path: '/learn', element: <LearnPage /> },
-    { path: '/content-detect', element: <ContentDetectPage /> },
-  ]
+    { path: '/content-detect', element: <ContentDetectPage /> }]
+  }]
+
 
   const router = createBrowserRouter(routes);
 
+
+function App() {
   return (
+    <>
     <RouterProvider router={router} />
+    </>
   );
 }
-
 
 export default App
